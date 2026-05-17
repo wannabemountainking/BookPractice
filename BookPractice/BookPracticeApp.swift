0e01dd6034abfa7191c70559057ceaec1823f71c
김도윤 <kdy0096@gmail.com>
2026-05-17 23:33:24 +0900

    Initial Commit

---
+++ BookPractice/BookPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookPracticeApp.swift
+//  BookPractice
+//
+//  Created by yoonie on 5/17/26.
+//
+
+import SwiftUI
+
+@main
+struct BookPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
